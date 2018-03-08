37f98c641b6bfb56e88ee25cd5803ab5ba83a1b6
qiukai <943451277@qq.com>
2018-03-08 17:10:36 +0800

    qk

--- src/utils/request.js
+++ src/utils/request.js
@@ -45,42 +45,8 @@ service.interceptors.response.use(response => {
       type: 'warning'
     }).then(() => {
       store.dispatch('FedLogOut').then(() => {
-        location.reload()// 为了重新实例化vue-router对象 避免bug
+        window.location.reload()// 为了重新实例化vue-router对象 避免bug
       })
-<<<<<<< HEAD
-=======
-      alert(res.code)
-      // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
-          confirmButtonText: '重新登录',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('FedLogOut').then(() => {
-            location.reload()// 为了重新实例化vue-router对象 避免bug
-          })
-        })
-      }
-      return Promise.reject('error')
-    } else {
-      return response.data
-    }
-    */
-    return res
-  },
-  error => {
-    console.log('err' + error)// for debug
-    // if (error === 'Network Error') {
-    //   error = '网络错误！'
-    // } else if (error === 'timeout of 30000ms exceeded') {
-    //   error = '请求超时，请检查您的网络！'
-    // }
-    Message({
-      message: error,
-      type: 'error',
-      duration: 5 * 1000
->>>>>>> afbfb7da4a9e0d1d2106a4e3b412102611cf810a
     })
   }
   return res
