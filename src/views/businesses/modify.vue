aa5a926c2141e828fa110fcb907d20b4742b3995
maanjun <ma.anjun@163.com>
2018-04-28 11:09:13 +0800

    修改售后处理点及修改个人信息问题

--- src/views/businesses/modify.vue
+++ src/views/businesses/modify.vue
@@ -1047,6 +1047,7 @@
       },
       getAddressInfo(addressInfo) {
         this.sellAddressListForm[addressInfo.index].locationcode = (addressInfo.id !== undefined ? addressInfo.id.toString() : '')
+        this.sellAddressListForm[addressInfo.index].code = (addressInfo.locationCode !== undefined ? addressInfo.locationCode : '')
         this.sellAddressListForm[addressInfo.index].selladdress = addressInfo.town_village
       },
       addSellAddress() {
@@ -1059,6 +1060,7 @@
         var sellAddress = {
           'id': this.sellAddressListForm.length,
           'locationcode': '',
+          'code': '',
           'selladdress': '',
           'merchantcode': '',
           'valid': true
@@ -1352,7 +1354,7 @@
         for (let i = 0; i < this.sellAddressListForm.length; i++) {
           if (this.sellAddressListForm[i].valid === true) {
             sellAddressList.push({
-              'locationcode': `${this.sellAddressListForm[i].locationcode}`,
+              'locationcode': `${this.sellAddressListForm[i].code}`,
               'selladdress': `${this.sellAddressListForm[i].selladdress}`
             })
           }
